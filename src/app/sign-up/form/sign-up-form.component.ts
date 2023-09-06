f541cd5ba96d89af4095977815b9dc75c97a2b51
Cesar Velásquez Córdova <ces.velasquez@hotmail.com>
2023-09-05 23:52:36 -0600

    feat(auth): close modal

--- src/app/sign-up/form/sign-up-form.component.ts
+++ src/app/sign-up/form/sign-up-form.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-sign-up-form',
   templateUrl: './sign-up-form.component.html',
-  styleUrls: ['./sign-up-form.component.scss']
+  styleUrls: ['./sign-up-form.component.scss'],
 })
 export class SignUpFormComponent {
+  constructor(readonly modalController: ModalController) {}
 
 }
