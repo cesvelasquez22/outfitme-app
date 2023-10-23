673b1ae2879a10a525f1ff8aa8ec7a6104219a82
Cesar Velásquez Córdova <ces.velasquez@hotmail.com>
2023-10-22 23:01:46 -0600

    ui(profiles): show profile avatar

--- src/app/pages/profiles/profiles.page.ts
+++ src/app/pages/profiles/profiles.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 interface Profile {
   id: number;
@@ -24,11 +24,24 @@ export class ProfilesPage implements OnInit {
       id: 3,
       profileName: 'John Smith',
     },
+    {
+      id: 4,
+      profileName: 'John Doe',
+    },
+    {
+      id: 5,
+      profileName: 'Jane Doe',
+    },
   ];
+
+  @ViewChild('headerEl') headerEl!: ElementRef;
+  @ViewChild('profilesEl') profilesEl!: ElementRef;
 
   constructor() {}
 
   ngOnInit() {}
+
+  onClick() {}
 
   trackByFn(index: number, item: Profile) {
     return item.id;
