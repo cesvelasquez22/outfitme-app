283686a93e7ebccc98c66d8a78f95765de0daa3c
Cesar Velásquez Córdova <ces.velasquez@hotmail.com>
2023-10-29 22:30:32 -0600

    ui(splash screen): add splash screen

--- src/app/@core/core.module.ts
+++ src/app/@core/core.module.ts
@@ -5,9 +5,10 @@ import { UserService } from './user/user.service';
 import { StorageService } from './services/storage';
 
 import { AuthModule } from './auth';
+import { SplashScreenModule } from './services/splash-screen';
 
 @NgModule({
-  imports: [HttpClientModule, AuthModule],
+  imports: [HttpClientModule, AuthModule, SplashScreenModule],
   providers: [StorageService, UserService],
 })
 export class CoreModule {}
